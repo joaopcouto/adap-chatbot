d562853ba25d2449a5f8930420e8e236df285c11
João Pedro Couto <129805937+joaopcouto@users.noreply.github.com>
2025-06-23 17:43:56 -0300

    Update Transaction.js

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -4,7 +4,7 @@ const transactionSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   amount: { type: Number, required: true },
   description: { type: String, required: true },
-  category: { type: String, required: true },
+  categoryId: { type: String, required: true },
   date: { type: Date, default: Date.now },
   messageId: { type: String, required: true },
   type: { type: String, required: true },
