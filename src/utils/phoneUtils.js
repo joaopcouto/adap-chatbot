03ed2984b49d8b9b113e1365f06afd7ccb07848a
João Pedro Couto <129805937+joaopcouto@users.noreply.github.com>
2025-07-22 22:24:06 -0300

    Update phoneUtils.js

--- src/utils/phoneUtils.js
+++ src/utils/phoneUtils.js
@@ -4,10 +4,8 @@ export function fixPhoneNumber(phoneNumber) {
   // Check if the number has 12 digits (one less than expected 13)
   if (numberOnly.length === 12) {
     // Insert '9' as the fifth character (after area code)
-    return `whatsapp:+${numberOnly.slice(0, 4)}9${numberOnly.slice(4)}`;
+    return `${numberOnly.slice(0, 4)}9${numberOnly.slice(4)}`;
   }
 
   return numberOnly;
 }
-
-
